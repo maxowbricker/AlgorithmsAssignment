51c2d199f66b36b1595dc3fba2cdd08800340d3d
maxowbricker <128575472+maxowbricker@users.noreply.github.com>
2023-09-05 17:55:33 +1000

    TRIE_UNTESTED_CODE_ADDED

--- dictionary/trie_dictionary.py
+++ dictionary/trie_dictionary.py
@@ -11,6 +11,10 @@ from dictionary.word_frequency import WordFrequency
 
 
 # Class representing a node in the Trie
+from dictionary.base_dictionary import BaseDictionary
+from dictionary.word_frequency import WordFrequency
+
+# Class representing a node in the Trie
 class TrieNode:
 
     def __init__(self, letter=None, frequency=None, is_last=False):
@@ -23,53 +27,88 @@ class TrieNode:
 class TrieDictionary(BaseDictionary):
 
     def __init__(self):
-        # TO BE IMPLEMENTED
-        pass
+        self.root = TrieNode()  # Initialize an empty Trie
 
     def build_dictionary(self, words_frequencies: [WordFrequency]):
         """
-        construct the data structure to store nodes
+        Construct the data structure to store nodes
         @param words_frequencies: list of (word, frequency) to be stored
         """
-        # TO BE IMPLEMENTED
-
+        for word, frequency in words_frequencies:
+            self.add_word_frequency(WordFrequency(word, frequency))
 
     def search(self, word: str) -> int:
         """
-        search for a word
+        Search for a word
         @param word: the word to be searched
         @return: frequency > 0 if found and 0 if NOT found
         """
-        # TO BE IMPLEMENTED
-
+        node = self._find_node(word)
+        if node and node.is_last:
+            return node.frequency
         return 0
-
 
     def add_word_frequency(self, word_frequency: WordFrequency) -> bool:
         """
-        add a word and its frequency to the dictionary
+        Add a word and its frequency to the dictionary
         @param word_frequency: (word, frequency) to be added
         :return: True whether succeeded, False when word is already in the dictionary
         """
+        word = word_frequency.word
+        frequency = word_frequency.frequency
+        node = self.root
 
-        # TO BE IMPLEMENTED
+        for letter in word:
+            if letter not in node.children:
+                node.children[letter] = TrieNode(letter)
+            node = node.children[letter]
 
-        return False
+        if node.is_last:
+            return False  # Word already exists in the dictionary
+
+        node.is_last = True
+        node.frequency = frequency
+        return True
 
     def delete_word(self, word: str) -> bool:
         """
-        delete a word from the dictionary
+        Delete a word from the dictionary
         @param word: word to be deleted
-        @return: whether succeeded, e.g. return False when point not found
+        @return: whether succeeded, e.g. return False when word not found
         """
-
+        node = self._find_node(word)
+        if node and node.is_last:
+            node.is_last = False
+            node.frequency = None
+            return True
         return False
-
 
     def autocomplete(self, word: str) -> [WordFrequency]:
         """
-        return a list of 3 most-frequent words in the dictionary that have 'word' as a prefix
+        Return a list of 3 most-frequent words in the dictionary that have 'word' as a prefix
         @param word: word to be autocompleted
         @return: a list (could be empty) of (at most) 3 most-frequent words with prefix 'word'
         """
-        return []
+        completions = []
+        node = self._find_node(word)
+
+        if node:
+            self._find_completions(node, word, completions)
+
+        completions.sort(key=lambda x: x.frequency, reverse=True)
+        return completions[:3]
+
+    def _find_node(self, word: str) -> TrieNode:
+        node = self.root
+        for letter in word:
+            if letter not in node.children:
+                return None
+            node = node.children[letter]
+        return node
+
+    def _find_completions(self, node: TrieNode, prefix: str, completions: [WordFrequency]):
+        if node.is_last:
+            completions.append(WordFrequency(prefix, node.frequency))
+
+        for letter, child_node in node.children.items():
+            self._find_completions(child_node, prefix + letter, completions
