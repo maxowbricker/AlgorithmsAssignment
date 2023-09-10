02cd4300a39d840e68ce9141628d4fd15cd69734
Dragan76 <s3965382@student.rmit.edu.au>
2023-09-10 22:42:15 +1000

    More run times

--- T3sT_RunT1mEs.py
+++ T3sT_RunT1mEs.py
@@ -16,14 +16,64 @@ def create_linkedlist_from_txt(filename):
 if __name__ == "__main__":
 
     filename = "sampleData.txt"
+    num_trials = 10
+    total_time = 0
+    total_timeB = 0
+    total_timeW = 0
+    total_timeDB = 0
+    total_timeDW = 0
 
-    start_time = time.time()
-    linked_list_dict = create_linkedlist_from_txt(filename)
-    end_time = time.time()
+    # Average run time for building a linked list dictionary
+    for _ in range(num_trials):
+        start_time = time.time()
+        linked_list_dict = create_linkedlist_from_txt(filename)
+        end_time = time.time()
 
+        total_time += (end_time - start_time)
 
-    print(f"Time taken =  {(end_time - start_time):.2f} sec")
+    avg_time = total_time / num_trials
+    print(f"Average time taken over {num_trials} trials =  {avg_time:.5f} sec")
 
-    # If you wish to test whether the LinkedListDictionary has been populated correctly:
-    # word = "skywalker"
-    # print(f"Frequency of '{word}' is: {linked_list_dict.search(word)}")
+    # Average best case run time for searching a word in the linked list dictionary
+    for _ in range(num_trials):
+        start_time = time.time()
+        freq = linked_list_dict.search('reclassify')
+        end_time = time.time()
+
+        total_timeB += (end_time - start_time)
+
+    avg_timeB = total_timeB / num_trials
+    print(f"Average time taken over {num_trials} trials =  {avg_timeB:.5f} sec")
+
+    # Average worst case run time for searching a word in the linked list dictionary
+    for _ in range(num_trials):
+        start_time = time.time()
+        freq = linked_list_dict.search('eregjergjerigjio')
+        end_time = time.time()
+
+        total_timeW += (end_time - start_time)
+
+    avg_timeW = total_timeW / num_trials
+    print(f"Average time taken over {num_trials} trials =  {avg_timeW:.5f} sec")
+
+    # Average best case run time for deleting a word in the linked list dictionary
+    for _ in range(num_trials):
+        start_time = time.time()
+        freq = linked_list_dict.delete_word('reclassify')
+        end_time = time.time()
+
+        total_timeDB += (end_time - start_time)
+
+    avg_timeDB = total_timeDB / num_trials
+    print(f"Average time taken over {num_trials} trials =  {avg_timeDB:.5f} sec")
+
+    # Average worst case run time for deleting a word in the linked list dictionary
+    for _ in range(num_trials):
+        start_time = time.time()
+        freq = linked_list_dict.delete_word('eregjergjerigjio')
+        end_time = time.time()
+
+        total_timeDW += (end_time - start_time)
+
+    avg_timeDW = total_timeDW / num_trials
+    print(f"Average time taken over {num_trials} trials =  {avg_timeDW:.5f} sec")
