6ba173777f83a6127e4552ec8d7c7204aaea60ba
Dragan76 <s3965382@student.rmit.edu.au>
2023-09-11 12:30:40 +1000

    linked-list runtime changes

--- linkedlist-test-runtime.py
+++ linkedlist-test-runtime.py
@@ -33,18 +33,21 @@ if __name__ == "__main__":
         total_time += (end_time - start_time)
 
     avg_time = total_time / num_trials
-    print(f"Average time taken over {num_trials} trials =  {avg_time:.5f} sec")
+    print(f"Average time taken over {num_trials} trials for building a dictionary =  {avg_time:.5f} sec")
+
+    lld_first = linked_list_dict.head.word_frequency.word
+    lld_first_auto = lld_first[:4]
 
     # Average best case run time for searching a word in the linked list dictionary
     for _ in range(num_trials):
         start_time = time.time()
-        freq = linked_list_dict.search('reclassify')
+        freq = linked_list_dict.search(lld_first)
         end_time = time.time()
 
         total_timeB += (end_time - start_time)
 
     avg_timeB = total_timeB / num_trials
-    print(f"Average time taken over {num_trials} trials =  {avg_timeB:.5f} sec")
+    print(f"Average best time taken over {num_trials} trials for searching for a word =  {avg_timeB:.5f} sec")
 
     # Average worst case run time for searching a word in the linked list dictionary
     for _ in range(num_trials):
@@ -55,18 +58,18 @@ if __name__ == "__main__":
         total_timeW += (end_time - start_time)
 
     avg_timeW = total_timeW / num_trials
-    print(f"Average time taken over {num_trials} trials =  {avg_timeW:.5f} sec")
+    print(f"Average worst time taken over {num_trials} trials for searching for a word =  {avg_timeW:.5f} sec")
 
     # Average best case run time for deleting a word in the linked list dictionary
     for _ in range(num_trials):
         start_time = time.time()
-        freq = linked_list_dict.delete_word('reclassify')
+        freq = linked_list_dict.delete_word(lld_first)
         end_time = time.time()
 
         total_timeDB += (end_time - start_time)
 
     avg_timeDB = total_timeDB / num_trials
-    print(f"Average time taken over {num_trials} trials =  {avg_timeDB:.5f} sec")
+    print(f"Average best time taken over {num_trials} trials for deleting a word =  {avg_timeDB:.5f} sec")
 
     # Average worst case run time for deleting a word in the linked list dictionary
     for _ in range(num_trials):
@@ -77,18 +80,18 @@ if __name__ == "__main__":
         total_timeDW += (end_time - start_time)
 
     avg_timeDW = total_timeDW / num_trials
-    print(f"Average time taken over {num_trials} trials =  {avg_timeDW:.5f} sec")
+    print(f"Average worst time taken over {num_trials} trials for deleting a word =  {avg_timeDW:.5f} sec")
 
     # Average best case run time for autocompleting a word in the linked list dictionary
     for _ in range(num_trials):
         start_time = time.time()
-        suggestions = linked_list_dict.autocomplete('rec')  # Use a prefix expected to be common for best case
+        suggestions = linked_list_dict.autocomplete(lld_first_auto)  # Use a prefix expected to be common for best case
         end_time = time.time()
 
         total_timeAB += (end_time - start_time)
 
     avg_timeAB = total_timeAB / num_trials
-    print(f"Average best-case time for autocomplete over {num_trials} trials = {avg_timeAB:.5f} sec")
+    print(f"Average best time taken for autocomplete over {num_trials} trials = {avg_timeAB:.5f} sec")
 
     # Average worst case run time for autocompleting a word in the linked list dictionary
     total_timeAW = 0  # Total time for autocomplete worst case
@@ -100,4 +103,4 @@ if __name__ == "__main__":
         total_timeAW += (end_time - start_time)
 
     avg_timeAW = total_timeAW / num_trials
-    print(f"Average worst-case time for autocomplete over {num_trials} trials = {avg_timeAW:.5f} sec")
+    print(f"Average worst time taken for autocomplete over {num_trials} trials = {avg_timeAW:.5f} sec")
